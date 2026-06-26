44f65d04453dff8458e10453ac441887e70f7653
valvicthe <louie12brett@icloud.com>
2026-06-26 13:45:05 +0100

    Create PlaylistsView.tsx

---
+++ src/components/Views/PlaylistsView.tsx
@@ -0,0 +1,12 @@
+export default function PlaylistsView() {
+  return (
+    <div className="w-full h-full font-mono text-[11px]">
+      <div className="mb-4 border-b border-zinc-900 pb-2">
+        <span className="text-zinc-500 font-bold">USER_PLAYLIST_MATRICES</span>
+      </div>
+      <div className="text-zinc-700 border border-zinc-900 border-dashed p-4 text-center">
+        WRITING_PLAYLIST_SYSTEM_DISABLED
+      </div>
+    </div>
+  );
+}
